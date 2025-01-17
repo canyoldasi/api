f9fabc3d344fa02351a8bf795d82898d6c187891
Esref Atak <esrefatak@gmail.com>
2025-01-17 18:15:21 +0300

    graphql ile roles field'ını resolve ettik

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,6 +1,8 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { InjectEntityManager } from "@nestjs/typeorm";
 import { Role } from "src/entities/role.entity";
+import { UserRole } from "src/entities/user-role.entity";
+import { User } from "src/models/user.model";
 import { EntityManager } from "typeorm";
 
 @Injectable()
@@ -9,10 +11,11 @@ export class RoleService {
         @InjectEntityManager()
         private entityManager: EntityManager
     ) {}
-    async findAll(): Promise<Role[]> {
-        return [
-            { id: 1, name: 'Admin', code: '' },
-            { id: 2, name: 'User', code: '' },
-          ];
+    async findAll(user: User): Promise<UserRole[]> {
+        return this.entityManager.findBy(UserRole, {
+            user: {
+                id: user.id
+            }
+        })
     }
 }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,6 +3,7 @@ import { Resolver, ResolveField, Args, Query, Int, Parent} from '@nestjs/graphql
 import { UserService } from "./user.service";
 import { RoleService } from "src/role/role.service";
 import { Role } from "src/models/role.model";
+import { UserRole } from "src/entities/user-role.entity";
 
 @Resolver(() => User)
 export class UserResolver {
@@ -11,13 +12,13 @@ export class UserResolver {
         private roleService: RoleService
     ) {}
 
-    @Query(() => User)
+    @Query(() => User, {nullable: true})
     async getUser(@Args('id', {type: () => Int}) id: number) {
         return this.userService.getOneById(id)
     }
 
     @ResolveField('roles', () => [Role])
-    async getUserRoles(@Parent() user: User): Promise<Role[]> {
-        return this.roleService.findAll()
+    async getUserRoles(@Parent() user: User): Promise<UserRole[]> {
+        return this.roleService.findAll(user)
     }
 }
