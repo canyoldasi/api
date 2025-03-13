fc78eefeadf957e17755a121b2ada59678432ff9
Saliha Atak <esrefatak@gmail.com>
2025-03-13 13:15:49 +0300

    global exception uyumlama

--- src/providers/global.exception-filter.ts
+++ src/providers/global.exception-filter.ts
@@ -4,6 +4,7 @@ import { ApolloError } from 'apollo-server-express';
 import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 import { AdminNotificationHelper } from './admin-notification.helper';
 import { FastifyReply } from 'fastify';
+import FastifyRequestCustom from './fastify-request-custom';
 
 @Catch()
 export class GlobalExceptionFilter implements GqlExceptionFilter, ExceptionFilter {
@@ -11,8 +12,8 @@ export class GlobalExceptionFilter implements GqlExceptionFilter, ExceptionFilte
 
     catch(exception: any, host: ArgumentsHost) {
         const isGraphQL = host.getType<GqlContextType>() === 'graphql';
-        const context = isGraphQL ? host.getArgs()[2] : host.switchToHttp().getRequest();
-        const requestId = context?.requestId || 'unknown'; // requestId'yi al
+        const context = isGraphQL ? host.getArgs()[2] : host.switchToHttp().getRequest<FastifyRequestCustom>();
+        const requestId = context?.requestId || 'unknown';
 
         if (isGraphQL) {
             return this.handleGraphQLException(exception, requestId);
@@ -28,25 +29,27 @@ export class GlobalExceptionFilter implements GqlExceptionFilter, ExceptionFilte
         this.logger.error(`Status: ${status} Message: ${message}`, exception.stack, requestId);
 
         if (exception.notify) {
-            if (process.env.NODE_ENV == 'production') {
+            if (process.env.NODE_ENV === 'production') {
                 const emailHelper = new AdminNotificationHelper();
                 emailHelper.sendEmail(`
-          Status: ${status}
-          Message: ${message}
-          Request ID: ${requestId}
-          Stack: ${exception.stack}`);
+                    Status: ${status}
+                    Message: ${message}
+                    Request ID: ${requestId}
+                    Stack: ${exception.stack}`);
             }
         }
+
         return new ApolloError(`${exception.name}: ${message}`, status.toString(), {
             message,
-            stack: process.env.NODE_ENV == 'development' ? exception.stack : null,
+            stack: process.env.NODE_ENV === 'development' ? exception.stack : null,
+            requestId,
         });
     }
 
     private handleHTTPException(exception: any, host: ArgumentsHost, requestId: string) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<FastifyReply>();
-        const request = ctx.getRequest();
+        const request = ctx.getRequest<FastifyRequestCustom>();
 
         const status = exception instanceof HttpException ? exception.getStatus() : 500;
         const message = exception?.message || 'Internal server error';
@@ -54,23 +57,23 @@ export class GlobalExceptionFilter implements GqlExceptionFilter, ExceptionFilte
         this.logger.error(`Status: ${status} Message: ${message}`, exception.stack, requestId);
 
         if (exception.notify) {
-            if (process.env.NODE_ENV == 'production') {
+            if (process.env.NODE_ENV === 'production') {
                 const emailHelper = new AdminNotificationHelper();
                 emailHelper.sendEmail(`
-          Status: ${status}
-          Message: ${message}
-          Request ID: ${requestId}
-          Stack: ${exception.stack}`);
+                    Status: ${status}
+                    Message: ${message}
+                    Request ID: ${requestId}
+                    Stack: ${exception.stack}`);
             }
         }
 
         return response.status(status).send({
-            requestId: requestId,
+            requestId,
             statusCode: status,
             timestamp: new Date().toISOString(),
             path: request.url,
             message,
-            stack: process.env.NODE_ENV == 'development' ? exception.stack : null,
+            stack: process.env.NODE_ENV === 'development' ? exception.stack : null,
         });
     }
 }
