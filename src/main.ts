cf6598b3cdd38991d8ebe5716098dfc5f82448b8
Saliha Atak <esrefatak@gmail.com>
2025-03-13 13:14:32 +0300

    fastify adapter kod refactoring

--- src/main.ts
+++ src/main.ts
@@ -7,24 +7,26 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import * as fs from 'fs';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        process.env.HTTPS == 'true'
-            ? new FastifyAdapter({
-                  https: {
-                      key: fs.readFileSync('./ssl.key', 'utf-8'),
-                      cert: fs.readFileSync('./ssl.cer', 'utf-8'),
-                  },
-                  bodyLimit: 50048576,
-              })
-            : new FastifyAdapter({ bodyLimit: 50048576 })
-    );
+    const fastifyAdapter = new FastifyAdapter({
+        bodyLimit: 50048576,
+        ...(process.env.HTTPS === 'true' && {
+            https: {
+                key: fs.readFileSync('./ssl.key', 'utf-8'),
+                cert: fs.readFileSync('./ssl.cer', 'utf-8'),
+            },
+        }),
+    });
+
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter);
+
+    // CORS ayarları
     app.enableCors({
         origin: process.env.NODE_ENV === 'development' ? '*' : 'https://app.agiletechlondon.com',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true,
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     });
+
     await app.listen(process.env.APP_PORT, '0.0.0.0');
 }
 bootstrap();
