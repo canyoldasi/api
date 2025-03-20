8d41de88212ada0f20e46d7f66a385af8413d8a0
Saliha Atak <esrefatak@gmail.com>
2025-03-20 18:59:21 +0300

    role_id ismine geçtim ve role_permission tablosunu sadeleştirdim

--- src/entities/role-permission.entity.ts
+++ src/entities/role-permission.entity.ts
@@ -1,17 +1,16 @@
-import { Column, Entity, ManyToOne, JoinColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Role } from './role.entity';
 import { ObjectType, Field } from '@nestjs/graphql';
-import { BaseEntity } from './base.entity';
 import { Permission } from '../constants';
 
 @Entity()
 @ObjectType()
-export class RolePermission extends BaseEntity {
-    @Column({ name: 'roleId' })
-    roleId: string;
+export class RolePermission {
+    @PrimaryGeneratedColumn('uuid')
+    @Field({ nullable: false })
+    id: string;
 
     @ManyToOne(() => Role, (role) => role.rolePermissions, { nullable: false })
-    @JoinColumn({ name: 'roleId' })
     @Field(() => Role)
     role: Role;
 
